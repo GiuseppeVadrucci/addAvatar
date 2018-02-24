b426b9a5ca1af1776fe9a34862c3ca1c7ff0eb6c
giuseppe90 <givadrucci@gmail.com>
2018-02-24 21:34:24 +0100

    Update addavatar.go

--- addavatar.go
+++ addavatar.go
@@ -1,9 +1,6 @@
 var images = []string{".png",".jpg",".jpeg",".gif",".ico"}
 func addAvatar(w http.ResponseWriter, r *http.Request) {
-        userName := getUserName(r)
-        if err != nil || userName == ""{
-                 http.Redirect(w, r, "/", 302)
-        }
+        
         file, header, err := r.FormFile("file")
         if err != nil {
                 fmt.Fprintf(w, "Mime File not valid", 500)
@@ -23,24 +20,20 @@ func addAvatar(w http.ResponseWriter, r *http.Request) {
         }
 
         defer file.Close()
-        out, err := os.Create("static/"+header.Filename)
+        out, err := os.Create("/"+header.Filename)
         if err != nil {
                 fmt.Fprintf(w, "Error",500)
                 return
         }
 
         defer out.Close()
-        _, err = db.Exec("UPDATE user SET avatarpath = ? WHERE username=?","/static/"+header.Filename, userName)
-        if err != nil {
-            http.Error(w, "Server error, unable to update your account.", 500)
-            return
-        }
+        
 
         // write the content from POST to the file
         _, err = io.Copy(out, file)
         if err != nil {
                 fmt.Fprintln(w, err)
         }
-          http.Redirect(w, r, "/", 302)
+        
         
 }
